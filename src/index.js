dad194546d0b05413f991125f5e4ba235a2ac5af
sudharaD <sudharad7@gmail.com>
2021-05-30 22:59:14 +0530

    seasons react app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         position => console.log(position),
+//         err => console.log(err)
+//     );
+//     return (
+//         <div>
+//             Hi there!
+//         </div>
+//     );
+// };
+
+class App extends React.Component {
+    
+    // constructor(props) {
+    //     super(props);
+
+    //     // this is the only time we do the direct assisgnment to this.state
+    //     this.state = { lat: null, errorMessage: '' };
+
+    // }
+
+    state = { lat: null, errorMessage: '' };
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        );
+    }
+
+    renderContent() {
+        if(this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+        if(!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat = {this.state.lat} />
+        }
+        return <Spinner message="Please, Accept location request" />;
+    }
+
+    render() {
+        return(
+            <div className="border red">{this.renderContent()}</div>
+        );
+    }
+    
+        
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.querySelector('#root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
